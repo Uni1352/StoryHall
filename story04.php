ca8798225552c55c8c733f4657d297322f02f10c
Uni <a5304186@gmail.com>
2019-05-14 10:27:11 +0800

    modify story04.php

--- story04.php
+++ story04.php
@@ -47,9 +47,24 @@
         <h2 class="container__title"></h2>
         <!-- <video src="./media/BitcoinAndMining.mp4" controls></video> -->
         <div class="container__item">
-            <h3 class="container__subtitle"></h3>
-            <p class="container__content"></p>
-            <p class="container__content"></p>
+            <h3 class="container__subtitle">區塊鍊投票</h3>
+            <p class="container__content">睿家身處的國家離總統大選的日子愈來愈近了，而如果這次中選會設計投票的動線不佳，可能導致投票當天，民眾一等就是一整個下午。因此，社會上開始有人在討論區塊練應用在整個投票流程是否可行。好奇的睿家決定一探究竟，整理了區塊鍊應用在投票的情境上，到底會有哪些部分可能產生影響。</p>
+            <p>優點：</p>
+            <ol class="container__list">
+                <li class="container__listitem">每一個節點都是平等的</li>
+                <li class="container__listitem">紀錄是可追溯的</li>
+                <li class="container__listitem">節省選舉人事成本和時間資源</li>
+            </ol>
+            <p>缺點：</p>
+            <ol class="container__list">
+                <li class="container__listitem">不安全</li>
+            </ol>
+            <p>過程：</p>
+            <ol class="container__list">
+                <li class="container__listitem">每一個節點都是平等的</li>
+                <li class="container__listitem">紀錄是可追溯的</li>
+                <li class="container__listitem">節省選舉人事成本和時間資源</li>
+            </ol>
         </div>
         <div style="margin: 0 auto">
             <a href="./test.php"><input type="button" value="開始測驗" id="test"></a>
